25fdd67a80a47a8589d5999eea95504714661f62
eldrid01 <matthew.d.eldridge@gmail.com>
2021-03-17 10:40:30 +0000

    Updated to use new base class for command line programs

--- src/main/java/org/cruk/ampliconseq/ExtractAmpliconRegions.java
+++ src/main/java/org/cruk/ampliconseq/ExtractAmpliconRegions.java
@@ -26,17 +26,11 @@ package org.cruk.ampliconseq;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.PrintStream;
-import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
@@ -62,7 +56,7 @@ import htsjdk.samtools.util.Interval;
  *
  * @author eldrid01
  */
-public class ExtractAmpliconRegions {
+public class ExtractAmpliconRegions extends CommandLineProgram {
     private static final Logger logger = LogManager.getLogger();
 
     private File bamFile;
@@ -80,37 +74,11 @@ public class ExtractAmpliconRegions {
     }
 
     /**
-     * Parse command line arguments.
-     *
-     * @param args
-     */
-    private void parseCommandLineArgs(String[] args) {
-
-        Options options = createOptions();
-
-        checkForHelp(args, options);
-
-        try {
-            CommandLineParser parser = new DefaultParser();
-            CommandLine commandLine = parser.parse(options, args);
-
-            extractOptionValues(commandLine);
-
-        } catch (ParseException e) {
-            System.err.println("Error parsing command-line arguments");
-            System.err.println();
-            System.err.println(e.getMessage());
-            printUsage(options, System.err);
-            System.exit(1);
-        }
-    }
-
-    /**
      * Configure command line options.
      *
      * @return
      */
-    private Options createOptions() {
+    protected Options createOptions() {
         Options options = new Options();
 
         Option option = new Option("h", "help", false, "Print command line options");
@@ -163,7 +131,7 @@ public class ExtractAmpliconRegions {
      * @param commandLine
      * @throws ParseException
      */
-    private void extractOptionValues(CommandLine commandLine) throws ParseException {
+    protected void extractOptionValues(CommandLine commandLine) throws ParseException {
         bamFile = (File) commandLine.getParsedOptionValue("bam");
         ampliconsFile = (File) commandLine.getParsedOptionValue("amplicons");
         if (commandLine.hasOption("maximum-distance")) {
@@ -176,51 +144,9 @@ public class ExtractAmpliconRegions {
     }
 
     /**
-     * Workaround to allow for a help message to be displayed to stdout without
-     * error or warning messages about missing arguments.
-     * 
-     * @param args
-     * @param options
+     * Main run method for extracting SAM records from a BAM file that match
+     * amplicon intervals.
      */
-    private void checkForHelp(String[] args, Options options) {
-        if (!options.hasLongOption("help"))
-            return;
-
-        // create a copy of the options with none set to be required
-        Options checkForHelpOptions = new Options();
-        for (Option option : options.getOptions()) {
-            checkForHelpOptions.addOption(option.getOpt(), option.getLongOpt(), option.hasArg(),
-                    option.getDescription());
-        }
-
-        try {
-            CommandLineParser parser = new DefaultParser();
-            CommandLine commandLine = parser.parse(checkForHelpOptions, args, true);
-            if (commandLine.hasOption("help")) {
-                printUsage(options, System.out);
-                System.exit(0);
-            }
-        } catch (ParseException e) {
-        }
-    }
-
-    /**
-     * Print help/usage.
-     *
-     * @param options
-     * @param stream
-     */
-    private void printUsage(Options options, PrintStream stream) {
-        PrintWriter out = new PrintWriter(new OutputStreamWriter(stream));
-        out.println();
-        HelpFormatter helpFormatter = new HelpFormatter();
-        String syntax = "java " + getClass().getName() + " [options]";
-        String description = "\nExtracts reads from a BAM file that correspond to targeted PCR/amplicion regions.\n\n";
-        helpFormatter.printHelp(out, 80, syntax, description, options, 4, 8, "", false);
-        out.println();
-        out.flush();
-    }
-
     private void run() {
         IOUtil.assertFileIsReadable(bamFile);
         IOUtil.assertFileIsReadable(ampliconsFile);
