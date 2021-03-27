283d08493f279a3e766b6b1e7e2c3af7ee5e7a62
eldrid01 <matthew.d.eldridge@gmail.com>
2021-03-27 11:03:07 +0000

    Template script for generating pileup counts.

---
+++ templates/pileup_counts.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for group in `sed 1d !{amplicon_groups} | cut -f8 | sort -un`
+do
+	awk -v group=${group} 'BEGIN { FS = "\t"; OFS = "\t" } $8 == group { print $2, $5 - 1, $6, $1 }' !{amplicon_groups} > targets.bed
+
+    pileup-counts \
+        --id !{id} \
+        --input !{id}.${group}.bam \
+        --intervals targets.bed \
+        --reference-sequence !{reference_sequence} \
+        --output !{id}.${group}.pileup.txt
+done
+
+awk 'NR == 1 || FNR > 1' !{id}.*.pileup.txt > !{id}.pileup.txt
