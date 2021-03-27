87be564faf461dd0ebfbb933dd309d9e95292fc5
eldrid01 <matthew.d.eldridge@gmail.com>
2021-03-27 09:52:53 +0000

    Template for extracting subset BAM files for each amplicon group

---
+++ templates/extract_amplicon_regions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for group in `sed 1d !{amplicon_groups} | cut -f8 | sort -un`
+do
+	awk -v group=${group} 'BEGIN { FS = "\t"; OFS = "\t" } $8 == group { print $2, $3 - 1, $4, $1 }' !{amplicon_groups} > amplicons.bed
+
+	extract-amplicon-regions \
+		--id !{id} \
+		--input !{bam} \
+		--intervals amplicons.bed \
+		--output !{id}.${group}.bam \
+		--coverage !{id}.${group}.amplicon_coverage.txt \
+		--maximum-distance 0 \
+		--require-both-ends-anchored \
+		--unmark-duplicate-reads
+done
+
