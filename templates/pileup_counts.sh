9e270cc42aac8e04da34a30d1372a4d1f4d67dfc
eldrid01 <matthew.d.eldridge@gmail.com>
2021-04-17 21:08:12 +0100

    Added 'set -e -o pipefail' so that the template script fails on first error, added maximum heap size setting for pileup-counts Java program

--- templates/pileup_counts.sh
+++ templates/pileup_counts.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+set -e -o pipefail
+
+
 for group in `sed 1d !{amplicon_groups} | cut -f8 | sort -un`
 do
     awk -v group=${group} 'BEGIN { FS = "\t"; OFS = "\t" } $8 == group { print $2, $5 - 1, $6, $1 }' !{amplicon_groups} > targets.bed
 
-    pileup-counts \
+    JAVA_OPTS="-Xmx4g" pileup-counts \
         --id !{id} \
         --input !{id}.${group}.bam \
         --intervals targets.bed \
