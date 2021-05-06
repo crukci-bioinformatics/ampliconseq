10316d6401eb72ff53aa4d6219e8367f28e5c12f
eldrid01 <matthew.d.eldridge@gmail.com>
2021-05-06 12:46:30 +0100

    Use echo -e and \t for tab characters when writing new column headings to variant output table

--- templates/vardict.sh
+++ templates/vardict.sh
@@ -52,6 +52,6 @@ gatk VariantsToTable \
     --genotype-fields DP \
     --asGenotypeFieldsToTake AD
 
-echo "ID	Amplicon	Chromosome	Position	Ref	Alt	Quality	Filters	Type	Multiallelic	Depth	Ref count	Alt count" > !{id}.variants.txt
+echo -e "ID\tAmplicon\tChromosome\tPosition\tRef\tAlt\tQuality\tFilters\tType\tMultiallelic\tDepth\tRef count\tAlt count" > !{id}.variants.txt
 awk 'BEGIN { FS = "\t"; OFS = "\t" } FNR > 1 && $1 != "NA" { split($11, ad, ","); print "!{id}", $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, ad[1], ad[2] }' !{id}.variant_table.txt >> !{id}.variants.txt
 
