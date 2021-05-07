5b01692559c34bfb3f906c1fa174406c007681f1
eldrid01 <matthew.d.eldridge@gmail.com>
2021-05-07 21:11:02 +0100

    Template script for new Ensembl VEP process

---
+++ templates/variant_effect_predictor.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+create_vep_input.R \
+    --variants !{variants} \
+    --output distinct_variants.vcf
+
+vep \
+    --input_file distinct_variants.vcf \
+    --format vcf \
+    --output_file distinct_variants.vep.vcf \
+    --vcf \
+    --offline \
+    --dir_cache !{vep_cache_dir} \
+    --species !{params.vepSpecies} \
+    --assembly !{params.vepAssembly} \
+    --buffer_size 100 \
+    --no_stats \
+    --dont_skip \
+    --pick \
+    --everything \
+    --minimal \
+    --allele_number 1 \
+    --no_escape
+
+vep_vcf_to_tabular.R \
+    --vcf distinct_variants.vep.vcf \
+    --output !{variant_annotations}
+
