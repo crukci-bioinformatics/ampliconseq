9ab971a73b4d9e0ccb83ec2c5b542dfaa63bbdc3
eldrid01 <matthew.d.eldridge@gmail.com>
2021-05-12 20:15:59 +0100

    Sort outputs based on chromosome order in reference sequence index

--- bin/add_specific_variants.R
+++ bin/add_specific_variants.R
@@ -29,6 +29,9 @@ option_list <- list(
   make_option(c("--specific-variants"), dest = "specific_variants_file",
               help = "Specific/known variants file (Sample, Chromosome, Position, Ref and Alt columns required)"),
 
+  make_option(c("--reference-sequence-index"), dest = "reference_sequence_index_file",
+              help = "Index file for the reference genome sequence used for chromosome sort order (expected to have .fai extension)"),
+
   make_option(c("--output"), dest = "output_file",
               help = "Output file containing all variants including rows for missing calls")
 )
@@ -39,11 +42,13 @@ opt <- parse_args(option_parser)
 samples_file <- opt$samples_file
 variants_file <- opt$variants_file
 specific_variants_file <- opt$specific_variants_file
+reference_sequence_index_file <- opt$reference_sequence_index_file
 output_file <- opt$output_file
 
 if (is.null(samples_file)) stop("Sample sheet file must be specified")
 if (is.null(variants_file)) stop("Variant calls file must be specified")
 if (is.null(specific_variants_file)) stop("Specific variants file must be specified")
+if (is.null(reference_sequence_index_file)) stop("Reference sequence index file must be specified")
 if (is.null(output_file)) stop("Output file must be specified")
 
 suppressPackageStartupMessages(library(tidyverse))
@@ -74,10 +79,18 @@ sample_variants <- sample_variants %>%
   mutate(Specific = replace_na(Specific, "false"))
 
 # add variant calls
-sample_variants %>%
+variants <- sample_variants %>%
   left_join(samples, by = "Sample") %>%
   left_join(variants, by = c("ID", "Amplicon", "Chromosome", "Position", "Ref", "Alt")) %>%
   select(Sample, ID, Amplicon, Chromosome, Position, Ref, Alt, everything()) %>%
-  mutate(Filters = replace_na(Filters, "not_called")) %>%
+  mutate(Filters = replace_na(Filters, "not_called"))
+
+# read reference genome index file
+chromosomes <- read_tsv(reference_sequence_index_file, col_types = "cnnnn", col_names = c("Chromosome", "Length", "Offset", "Linebases", "Linewidth"))
+
+# sort variants and write to output file
+variants %>%
+  mutate(Chromosome = factor(Chromosome, levels = chromosomes$Chromosome)) %>%
+  arrange(Sample, Chromosome, Position, Ref, Alt, Amplicon, ID) %>%
   write_tsv(output_file, na = "")
-  
+
