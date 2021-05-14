1eb04abd496493ee329e2b868b6c23ccf3375c0f
eldrid01 <matthew.d.eldridge@gmail.com>
2021-05-14 18:04:00 +0100

    Rename column headings and add alleles column

--- templates/annotate_variants.sh
+++ templates/annotate_variants.sh
@@ -15,7 +15,7 @@ add-assorted-annotations-to-vcf \
 
 gatk --java-options "-Xmx!{java_mem}m" VariantsToTable \
     --variant distinct_variants.annotated.vcf \
-    --output !{variant_annotations} \
+    --output distinct_variants.annotated.txt \
     --fields CHROM \
     --fields POS \
     --fields REF \
@@ -23,4 +23,7 @@ gatk --java-options "-Xmx!{java_mem}m" VariantsToTable \
     --fields FivePrimeContext \
     --fields ThreePrimeContext \
     --fields IndelLength
+
+echo -e "Chromosome\tPosition\tRef\tAlt\t5' context\tAlleles\t3' context\tIndel length" > !{variant_annotations}
+awk 'BEGIN { FS = "\t"; OFS = "\t" } FNR > 1 { print $1, $2, $3, $4, $5, $3"/"$4, $6, $7 }' distinct_variants.annotated.txt >> !{variant_annotations}
 
