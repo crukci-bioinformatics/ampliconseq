ae872b1d52e68a06458fccbf4404b46a8863b431
eldrid01 <matthew.d.eldridge@gmail.com>
2025-11-20 15:43:48 +0000

    Left-align and normalize indels with bcftools norm, replaces splitting of multi-allelic variants in subsequent GATK VariantsToTable call

--- templates/collate_variants.sh
+++ templates/collate_variants.sh
@@ -14,22 +14,29 @@ gatk --java-options "-Xmx!{java_mem}m" MergeVcfs \
 
 add-assorted-annotations-to-vcf \
     --input "!{vcf}" \
-    --output merged_and_annotated_variants.vcf \
+    --output merged_annotated.vcf \
     --reference-sequence !{reference_sequence} \
     --sequence-context-length 1
 
+# left-align and normalize indels and split multi-allelic variants
+# note that this can still result in deletions that have an '*' for the
+# alt allele which will need fixing
+bcftools norm \
+    --multiallelics -both \
+    --fasta-ref !{reference_sequence} \
+    merged_annotated.vcf \
+    --output merged_annotated_normalized.vcf
+
 gatk --java-options "-Xmx!{java_mem}m" VariantsToTable \
-    --variant merged_and_annotated_variants.vcf \
-    --output merged_and_annotated_variants.txt \
+    --variant merged_annotated_normalized.vcf \
+    --output merged_annotated_normalized.txt \
     --show-filtered \
-    --split-multi-allelic \
     --fields AMPLICON \
     --fields CHROM \
     --fields POS \
     --fields REF \
     --fields ALT \
-    --fields MULTI-ALLELIC \
-    --fields TYPE \
+    --fields MULTIALLELIC \
     --fields FILTER \
     --fields QUAL \
     --fields FivePrimeContext \
@@ -38,7 +45,7 @@ gatk --java-options "-Xmx!{java_mem}m" VariantsToTable \
     --asGenotypeFieldsToTake AF
 
 tidy_variant_table.R \
-    --input merged_and_annotated_variants.txt \
+    --input merged_annotated_normalized.txt \
     --id "!{id}" \
     --output "!{variants}"
 
